88590587cdf7a1924cea8e7414f5facd8a845644
xichen1997 <chenxi.edu1997@gmail.com>
2025-03-23 16:24:13 -0400

    First version of full-instruction supported 6502 CPU

---
+++ build/CMakeFiles/6502cpu.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/xichen/dev/6502CPU/CPU65C02.cpp" "CMakeFiles/6502cpu.dir/CPU65C02.cpp.o" "gcc" "CMakeFiles/6502cpu.dir/CPU65C02.cpp.o.d"
+  "/Users/xichen/dev/6502CPU/main.cpp" "CMakeFiles/6502cpu.dir/main.cpp.o" "gcc" "CMakeFiles/6502cpu.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
